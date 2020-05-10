d0ba3d45833671e0668b2634004941848426ea0e
abc1196 <alejob_11@hotmail.com>
2020-05-09 23:38:19 -0500

    Change file type from directive to validator

--- src/app/components/add-movie/add-movie.component.ts
+++ src/app/components/add-movie/add-movie.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { fileTypeValidator } from 'src/app/directives/file-type.directive';
+import { fileTypeValidator } from 'src/app/validators/file-type.validator';
 import { FileService } from 'src/app/services/file.service';
 import { take } from 'rxjs/operators';
 import { MovieService } from 'src/app/services/movie.service';
--- src/app/directives/file-type.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FileTypeDirective } from './file-type.directive';
-
-describe('FileTypeDirective', () => {
-  it('should create an instance', () => {
-    const directive = new FileTypeDirective();
-    expect(directive).toBeTruthy();
-  });
-});

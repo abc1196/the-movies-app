9b3fb4677a4de84135925ee798148353c5b8d397
abc1196 <alejob_11@hotmail.com>
2020-05-09 23:35:49 -0500

    Add error service

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -3,8 +3,8 @@ import { Movie } from '../data/movie';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of, Subject } from 'rxjs';
 import { catchError, tap, map } from 'rxjs/operators';
-import { MessageService } from './message.service';
 import { MoviesWrapper } from '../data/moviesWrapper';
+import { ErrorService } from './error.service';
 
 @Injectable({
   providedIn: 'root',
@@ -14,27 +14,9 @@ export class MovieService {
 
   movie$: Subject<Movie> = new Subject<Movie>();
 
-  movies: Movie[] = [
-    {
-      title: 'Avengers Endgame',
-      release: new Date(),
-      image:
-        'https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_UX182_CR0,0,182,268_AL_.jpg',
-      description: '',
-    },
-    {
-      title: 'Avengers Endgame',
-      release: new Date(),
-      image:
-        'https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_UX182_CR0,0,182,268_AL_.jpg',
-      description: '',
-    },
-  ];
+  movies: Movie[] = [];
 
-  constructor(
-    private http: HttpClient,
-    private messageService: MessageService
-  ) {}
+  constructor(private http: HttpClient, private errorService: ErrorService) {}
 
   getMovie(): Observable<Movie> {
     return this.movie$.asObservable();
@@ -60,26 +42,7 @@ export class MovieService {
   getTopMovies(): Observable<Movie[]> {
     return this.http.get<MoviesWrapper>(this.topMoviesUrl).pipe(
       map((response) => response.movies),
-      catchError(this.handleError<Movie[]>('getHeroes', []))
+      catchError(this.errorService.handleError<Movie[]>('getHeroes', []))
     );
-  }
-
-  /**
-   * Handle Http operation that failed.
-   * Let the app continue.
-   * @param operation - name of the operation that failed
-   * @param result - optional value to return as the observable result
-   */
-  private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
-
-      // TODO: better job of transforming error for user consumption
-      this.messageService.showError(`${operation} failed: ${error.message}`);
-
-      // Let the app keep running by returning an empty result.
-      return of(result as T);
-    };
   }
 }
